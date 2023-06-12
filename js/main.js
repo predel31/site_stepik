cf84ef18058198c96c107221e31f1b32b95e7329
predel31 <predel31@mail.ru>
2023-06-12 16:12:15 +0300

    Плавная прокрутка меню

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    /* Плавная прокрутка меню */
+    $('nav a[href^="#"]').click(function() {
+        let target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        },500);
+        $('nav a[href^="#"]').parent().removeClass('active');
+        $(this).parent().addClass('active');
+        return false;
+    });
+});
